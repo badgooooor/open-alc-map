67ce3ea58ab0476fb73cdbe79798f2045e0c5ee7
Yuttakhan B <yuttakhanbaingen@gmail.com>
2023-06-16 15:16:56 +0700

    draft: Add placeholder of information in popup and TODO

--- src/components/PopoverCard.svelte
+++ src/components/PopoverCard.svelte
@@ -8,7 +8,13 @@
   });
 </script>
 
-<div class="absolute inset-y-0 right-0 w-64 h-72 mx-72 my-72 rounded shadow-lg bg-white p-2">
+<div class="absolute inset-y-0 right-0 w-64 h-72 mx-72 my-72 rounded shadow-lg bg-white p-2 flex flex-col">
   <h2>{selectedProvinceValue}</h2>
-  <div>Some list of liquors from this province</div>
+  <hr />
+  <!-- Some list of liquors from selected province, TODO: Add row with information provided -->
+  <div class="grow flex flex-col overflow-auto">
+    <div class="mb-2">หมาใจดำ</div>
+    <div class="mb-2">Raiyoddoy</div>
+    <div class="mb-2">KILO Spirits</div>
+  </div>
 </div>
